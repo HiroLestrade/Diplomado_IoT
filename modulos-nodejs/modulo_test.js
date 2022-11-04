c5dd355c66623f94d7bd901270ce3e8576257cbb
Les <les@CTRL-PC.localdomain>
2022-11-04 12:24:46 -0600

    Módulos de Nodejs

---
+++ modulos-nodejs/modulo_test.js
@@ -0,0 +1,3 @@
+var calc = require('./modulo2.js');
+
+console.log(`Suma: 20 + 30 = ${calc.mult(20,30)}`);
---
+++ modulos-nodejs/suma.js
@@ -0,0 +1,5 @@
+var calc = {};
+
+calc.add = (a,b) => a + b;
+
+console.log(`Suma: 10 + 10 = ${calc.add(10,10)}`);
